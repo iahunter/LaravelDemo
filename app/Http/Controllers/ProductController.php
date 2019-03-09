cf3180c6590cc794c7fda3bbc939b718fa7ec241
vagrant <vagrant@laraveldemo>
2019-03-09 21:05:15 +0000

    Added API Product Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductResource; 
 use App\Model\Product;
 use Illuminate\Http\Request;
 
@@ -47,7 +48,11 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+		// Get specific Product without custom API resource
+		//return $product; 
+		
+        // Get specific Product by ID using custom API resource
+		return new ProductResource($product); 
     }
 
     /**
