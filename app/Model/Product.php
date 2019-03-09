2034e9745df8f9ee8c83fcb59421a3fddd53a360
vagrant <vagrant@laraveldemo>
2019-03-09 19:37:20 +0000

    Created Fake Data using Faker and Seeders. Had to add some uuid support in the Models to create the UUID during creating

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,29 @@
 
 namespace App\Model;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $guarded = []; // YOLO
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->{$product->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,29 @@
 
 namespace App\Model;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $guarded = []; // YOLO
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($review) {
+            $review->{$review->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
